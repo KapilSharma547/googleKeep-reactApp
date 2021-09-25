0277560304ee6ec4ffebdd611fc41ce400edee10
Kapil Sharma <kapilsharmanfl97@gmail.comgit config --global user.email kapilsharmanfl97@gmail.com>
2021-09-25 15:08:40 +0530

    google-keep app added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>@program create by kapil</p>
+    </div>
+  );
+}
+
+export default Footer;
